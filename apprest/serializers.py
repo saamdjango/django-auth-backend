7dbe95b6c7b638f5d4588de17666b4f37fb5a115
saamdjango <saam.django@gmail.com>
2026-01-12 15:48:00 +0530

    Fix email retrieval in user creation

--- apprest/serializers.py
+++ apprest/serializers.py
@@ -18,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data("email"),
+            email=validated_data["email"],
             password=validated_data["password"]
         )
         # user.set_password(validated_data['password'])
